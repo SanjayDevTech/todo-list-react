b5da8d40c47f750b1146b0026edae826315aab7d
Sanjay Developer <sureshsanjay805@gmail.com>
2020-08-10 09:38:25 +0530

    Updates

---
+++ precache-manifest.0368805a9d5432182cbeb5f4632ed7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb625cd3c1628c6ee492bb2a7b16b33",
+    "url": "/SanjayDevTech/todo-list-react/index.html"
+  },
+  {
+    "revision": "e71e10de29855a7967c3",
+    "url": "/SanjayDevTech/todo-list-react/static/css/main.2f3b4e3f.chunk.css"
+  },
+  {
+    "revision": "648be9838802c61f0071",
+    "url": "/SanjayDevTech/todo-list-react/static/js/2.7ae2ef7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SanjayDevTech/todo-list-react/static/js/2.7ae2ef7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71e10de29855a7967c3",
+    "url": "/SanjayDevTech/todo-list-react/static/js/main.bae2063d.chunk.js"
+  },
+  {
+    "revision": "5cce6f2f7d26576ed6bc",
+    "url": "/SanjayDevTech/todo-list-react/static/js/runtime-main.dd738955.js"
+  }
+]);
